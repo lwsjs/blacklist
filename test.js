c1d105ff88b094bf66a4f6f4ca3852fea9d30ae8
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-06-03 12:29:42 +0100

    upgrade lws

--- test.js
+++ test.js
@@ -8,30 +8,28 @@ const tom = module.exports = new Tom('blacklist')
 
 tom.test('simple', async function () {
   const port = 8000 + this.index
-  const lws = new Lws()
-  const server = lws.listen({
+  const lws = Lws.create({
     port,
     stack: Blacklist,
     blacklist: '/one'
   })
   const response = await fetch(`http://localhost:${port}/one`)
   const response2 = await fetch(`http://localhost:${port}/two`)
-  server.close()
+  lws.server.close()
   a.strictEqual(response.status, 403)
   a.strictEqual(response2.status, 404)
 })
 
 tom.test('wildcard', async function () {
   const port = 8000 + this.index
-  const lws = new Lws()
-  const server = lws.listen({
+  const lws = Lws.create({
     port,
     stack: Blacklist,
     blacklist: '/(.*).php'
   })
   const response = await fetch(`http://localhost:${port}/one.php`)
   const response2 = await fetch(`http://localhost:${port}/two`)
-  server.close()
+  lws.server.close()
   a.strictEqual(response.status, 403)
   a.strictEqual(response2.status, 404)
 })
